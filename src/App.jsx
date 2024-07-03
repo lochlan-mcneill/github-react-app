e77c5856f3b02f4e7817c4feb9e4379064cde848
Lochlan <lochlanmcneill0@gmail.com>
2024-07-03 10:13:48 +0100

    First Commit
    + Added necessary components for first Commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>This is my first React App on Github!</p>
+    </>
+  )
+}
+
+
+
+export default App
